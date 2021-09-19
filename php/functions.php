ccf103816945d0d530716f028cbdbd0b673bbd98
Simon Taylor <38040141+WhiskyDickens@users.noreply.github.com>
2021-09-19 11:43:15 +0100

    functions - php

---
+++ php/functions.php
@@ -0,0 +1,52 @@
+<?php
+
+  /**
+   * Hold global functions for the application
+   */
+
+   /**
+    * Parse JS fetch post (Raw WebKitFormBoundary data)
+    *
+    * @param array $a_data Raw WebKitFormBoundary Data to parse
+    * @return void
+    */
+  function parse_raw_http_request(array &$data) {
+    // read incoming data
+    $input = file_get_contents('php://input');
+    preg_match_all('/name="(.*)"\s+(\d+)/', $input, $submission);
+    unset($submission[0]);
+    // Get field keys
+    $fields = [];
+    foreach($submission[1] as $key => $field_name) {
+      $fields[$field_name] = $submission[2][$key];
+    }
+    return $fields;
+  }
+
+  /**
+   * Check form submission for required fields
+   *
+   * @param array $requirements An array of required field keys
+   * @return void
+   */
+  function validate_inputs($request, $requirements) {
+    $missing = [];
+    $empty = [];
+    // Foreach required field in requirements array
+    foreach($requirements as $key) {
+      // Check for missing
+      if(!isset($request[$key])) {
+        $missing[] = "$key missing";
+      } elseif($request[$key] == "") {
+        $empty[] = "$key empty";
+      }
+    }
+    // Return result
+    if(!empty($missing)) {
+      return $missing;
+    } elseif(!empty($empty)) {
+      return $empty;
+    } else {
+      return false;
+    }
+  }
